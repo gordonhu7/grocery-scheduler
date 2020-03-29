c931c69b8eeb1262ae0c3422cb502870c0677db1
gordonh <gordonh@qualtrics.com>
2020-03-29 13:34:41 -0700

    i think i was working in the wrong directory

--- src/app.js
+++ src/app.js
@@ -2,23 +2,13 @@
 var express = require('express')
 var app = express()
 
+require('./healthcheck/healthcheck-endpoint')(app)
+require('./sample-grocery-session/sample-grocery-session-endpoint')(app)
+
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
   res.send('goodbye coronavirus') 
   // maybe homepage here or smth
-})
-
-// wooooo checking if the app is alive
-app.get('/healthcheck', function (req, res) {
- 	res.status(200);
-	res.json({ working: true });
-	res.end(); // wtf do we need this?
-})
-
-// probably should define seperate files for each endpoint as logic
-// might get a bit complicated 
-app.get('/schedule-grocery-session', function (req, res) {
-  res.send('whole foods sucks') 
 })
 
 //Launch listening server on port 8081
