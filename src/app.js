73db207dc1f999beb607c93c4d1231ebed834de7
gordonh <gordonh@qualtrics.com>
2020-03-29 20:28:53 -0700

    moved homepage somewhere else

--- src/app.js
+++ src/app.js
@@ -2,14 +2,15 @@
 var express = require('express')
 var app = express()
 
+// importing the endpoints as routes to the app
 require('./healthcheck/healthcheck-endpoint')(app)
+require('./homepage/homepage-endpoint')(app)
 require('./sample-grocery-session/sample-grocery-session-endpoint')(app)
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('goodbye coronavirus') 
-  // maybe homepage here or smth
-})
+//example of how to define an endpoint
+//app.get('/', function (req, res) {
+//  res.send('goodbye coronavirus') 
+//})
 
 //Launch listening server on port 8081
 app.listen(8000, function () {
